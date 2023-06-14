69963a647d19bcafe2db360e7521f85b8037120d
kipkoechke <chepkwonyke1@gmail.com>
2023-06-14 08:47:33 +0300

    Worked on login page

---
+++ login_app/lib/features/authentication/screens/login/widgets/login_form.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:login_app/constants/sizes.dart';
+import 'package:login_app/constants/text_strings.dart';
+
+class LoginForm extends StatelessWidget {
+  const LoginForm({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Container(
+        padding: const EdgeInsets.symmetric(vertical: bFormHeight - 10),
+        child: Column(
+          children: [
+            TextFormField(
+              decoration: const InputDecoration(
+                prefixIcon: Icon(Icons.email_rounded),
+                label: Text(bEmail),
+              ),
+            ),
+            const SizedBox(height: bFormHeight - 20),
+            TextFormField(
+              decoration: const InputDecoration(
+                prefixIcon: Icon(Icons.password_rounded),
+                label: Text(bPassword),
+                suffixIcon: Icon(Icons.remove_red_eye_rounded),
+              ),
+            ),
+            const SizedBox(height: bFormHeight - 20),
+            Align(
+              alignment: Alignment.centerRight,
+              child: TextButton(
+                onPressed: () {},
+                child: Text(bForgetPassword.toUpperCase()),
+              ),
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text(
+                  bLogin.toUpperCase(),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
