020231404d7286a96e4565d2259574456ebb3ae4
kipkoechke <chepkwonyke1@gmail.com>
2023-06-14 08:47:11 +0300

    Worked on a custom form header for both signup and login screen

---
+++ login_app/lib/common_widgets/form/form_header_widget.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class FormHeaderWidget extends StatelessWidget {
+  const FormHeaderWidget({
+    super.key,
+    required this.image,
+    required this.title,
+    required this.subtitle,
+  });
+
+  final String image, title, subtitle;
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Image(
+          image: AssetImage(image),
+          height: size.height * 0.2,
+        ),
+        Text(
+          title,
+          style: Theme.of(context).textTheme.displayLarge,
+        ),
+        Text(
+          subtitle,
+          style: Theme.of(context).textTheme.bodyLarge,
+        ),
+      ],
+    );
+  }
+}
