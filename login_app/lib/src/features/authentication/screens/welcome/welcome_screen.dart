20d7f7e68f3638a723888ad6bf406f6dce484f1e
kipkoechke <chepkwonyke1@gmail.com>
2023-06-14 16:17:43 +0300

    Removed animation

--- login_app/lib/src/features/authentication/screens/welcome/welcome_screen.dart
+++ login_app/lib/src/features/authentication/screens/welcome/welcome_screen.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:login_app/src/common_widgets/fade_in_animation/animation_design.dart';
 import 'package:login_app/src/common_widgets/fade_in_animation/fade_in_animation_controller.dart';
-import 'package:login_app/src/common_widgets/fade_in_animation/fade_in_animation_model.dart';
 import 'package:login_app/src/constants/colors.dart';
 import 'package:login_app/src/constants/image_strings.dart';
 import 'package:login_app/src/constants/sizes.dart';
@@ -25,67 +23,53 @@ class WelcomeScreen extends StatelessWidget {
 
     return Scaffold(
         backgroundColor: isDark ? bSecondaryColor : bPrimaryColor,
-        body: Stack(
-          children: [
-            BFadeInAnimation(
-              durationIMls: 1200,
-              animate: BAnimatedPosition(
-                  topAfter: 0,
-                  topBefore: 0,
-                  leftAfter: 0,
-                  leftBefore: 0,
-                  bottomAfter: 0,
-                  bottomeBefore: -100,
-                  rightAfter: 0,
-                  rightBefore: 0),
-              child: Container(
-                padding: const EdgeInsets.all(bDefaultSize),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        body: Stack(children: [
+          Container(
+            padding: const EdgeInsets.all(bDefaultSize),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Image(
+                  image: AssetImage(bWelcomeScreenImage),
+                  // height: height * 0.6,
+                ),
+                Column(
                   children: [
-                    const Image(
-                      image: AssetImage(bWelcomeScreenImage),
-                      // height: height * 0.6,
+                    Text(
+                      bWelcomeTitle,
+                      style: Theme.of(context).textTheme.displaySmall,
                     ),
-                    Column(
-                      children: [
-                        Text(
-                          bWelcomeTitle,
-                          style: Theme.of(context).textTheme.displaySmall,
-                        ),
-                        Text(
-                          bWelcomeSubtitle,
-                          style: Theme.of(context).textTheme.bodyLarge,
-                          textAlign: TextAlign.center,
-                        ),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Expanded(
-                          child: OutlinedButton(
-                            onPressed: () {
-                              Get.to(() => const LoginScreen());
-                            },
-                            child: Text(bLogin.toUpperCase()),
-                          ),
-                        ),
-                        const SizedBox(width: 10),
-                        Expanded(
-                          child: ElevatedButton(
-                            onPressed: () {
-                              Get.to(() => const SignUpScreen());
-                            },
-                            child: Text(bSignup.toUpperCase()),
-                          ),
-                        ),
-                      ],
+                    Text(
+                      bWelcomeSubtitle,
+                      style: Theme.of(context).textTheme.bodyLarge,
+                      textAlign: TextAlign.center,
                     ),
                   ],
                 ),
-              ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: OutlinedButton(
+                        onPressed: () {
+                          Get.to(() => const LoginScreen());
+                        },
+                        child: Text(bLogin.toUpperCase()),
+                      ),
+                    ),
+                    const SizedBox(width: 10),
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Get.to(() => const SignUpScreen());
+                        },
+                        child: Text(bSignup.toUpperCase()),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
-          ],
-        ));
+          ),
+        ]));
   }
 }
