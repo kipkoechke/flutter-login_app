c3bfafd56e882d3607d86ae68655681805a163b2
kipkoechke <chepkwonyke1@gmail.com>
2023-06-16 17:30:48 +0300

    Worked on authentication logic

--- login_app/lib/src/repository/authentication_repository/authentication_reposirtory.dart
+++ login_app/lib/src/repository/authentication_repository/authentication_reposirtory.dart
@@ -1,13 +1,18 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
+import 'package:login_app/src/features/admin/dashboard/admin_dashboard.dart';
 import 'package:login_app/src/features/application/application_screen.dart';
 import 'package:login_app/src/features/authentication/screens/splash_screen/splash_screen.dart';
+import 'package:login_app/src/features/authentication/screens/welcome/welcome_screen.dart';
+import 'package:login_app/src/repository/authentication_repository/exceptions/signup_email_password_failure.dart';
 
 class AuthenticationRepository extends GetxController {
   static AuthenticationRepository get instance => Get.find();
 
   final _auth = FirebaseAuth.instance;
   late final Rx<User?> firebaseUser;
+  var verificationId = ''.obs;
+  int? resendToken;
 
   @override
   void onReady() {
@@ -17,40 +22,72 @@ class AuthenticationRepository extends GetxController {
   }
 
   _setInitialScreen(User? user) {
-    user == null
-        ? Get.off(() => const SplashScreen())
-        : Get.offAll(() => const ApplicationScreen());
+    if (user == null) {
+      Get.offAll(() => const SplashScreen());
+    } else if (user.email == 'admin@admin.com') {
+      Get.offAll(() => const AdminDashboardScreen());
+    } else {
+      Get.offAll(() => const ApplicationScreen());
+    }
+  }
+
+  Future<void> phoneAuthentication(String phoneNo) async {
+    await _auth.verifyPhoneNumber(
+      phoneNumber: phoneNo,
+      verificationCompleted: (credential) async {
+        await _auth.signInWithCredential(credential);
+      },
+      codeSent: (verificationId, resendToken) {
+        this.verificationId.value == verificationId;
+        this.resendToken == resendToken;
+      },
+      timeout: const Duration(seconds: 30),
+      forceResendingToken: resendToken,
+      codeAutoRetrievalTimeout: (verificationId) {
+        this.verificationId.value = verificationId;
+      },
+      verificationFailed: (e) {
+        if (e.code == 'invalid-phone-number') {
+          Get.snackbar('Error', 'The provided phone number is not valid.');
+          Get.back();
+        } else {
+          Get.snackbar('Error', 'Something went wrong. Try again.');
+          Get.back;
+        }
+      },
+    );
+  }
+
+  Future<bool> verifyOTP(String otp) async {
+    var credentials = await _auth.signInWithCredential(
+      PhoneAuthProvider.credential(
+          verificationId: verificationId.value, smsCode: otp),
+    );
+    return credentials.user != null ? true : false;
   }
 
   Future<void> createUserWithEmailAndPassword(
       String email, String password) async {
     try {
-      final credential = await _auth.createUserWithEmailAndPassword(
-        email: email,
-        password: password,
-      );
+      await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      firebaseUser.value != null
+          ? Get.offAll(() => const ApplicationScreen())
+          : Get.to(() => const WelcomeScreen());
     } on FirebaseAuthException catch (e) {
-      if (e.code == 'weak-password') {
-        print('The password provided is too weak.');
-      } else if (e.code == 'email-already-in-use') {
-        print('The account already exists for that email.');
-      }
-    } catch (e) {
-      print(e);
+      final ex = SignUpWithEmailAndPasswordFailure.code(e.code);
+      throw ex;
+    } catch (_) {
+      final ex = SignUpWithEmailAndPasswordFailure();
+      throw ex;
     }
   }
 
-  Future<void> loginWithEmailAndPassword(String email, String password) async {
+  Future<void> loginUserWithEmailAndPassword(
+      String email, String password) async {
     try {
-      final credential = await _auth.signInWithEmailAndPassword(
-          email: email, password: password);
-    } on FirebaseAuthException catch (e) {
-      if (e.code == 'user-not-found') {
-        print('No user found for that email.');
-      } else if (e.code == 'wrong-password') {
-        print('Wrong password provided for that user.');
-      }
-    }
+      await _auth.signInWithEmailAndPassword(email: email, password: password);
+    } catch (_) {}
   }
 
   Future<void> logOut() async => await _auth.signOut();
