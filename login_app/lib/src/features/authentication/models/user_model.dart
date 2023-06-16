72aee08053c66c145b689bbbaaf6b9df5c043bb3
kipkoechke <chepkwonyke1@gmail.com>
2023-06-16 17:37:41 +0300

    Added a user model for signup functionality

---
+++ login_app/lib/src/features/authentication/models/user_model.dart
@@ -0,0 +1,39 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class UserModel {
+  final String? id;
+  final String fullName;
+  final String email;
+  final String phoneNo;
+  final String password;
+
+  UserModel({
+    this.id,
+    required this.fullName,
+    required this.email,
+    required this.phoneNo,
+    required this.password,
+  });
+
+  toJson() {
+    return {
+      "FullName": fullName,
+      "Email": email,
+      "Phone Number": phoneNo,
+      "Password": password,
+    };
+  }
+
+  //-- Mapping user fetched from firebase to usermodel
+  factory UserModel.fromSnapshot(
+      DocumentSnapshot<Map<String, dynamic>> document) {
+    final data = document.data()!;
+    return UserModel(
+      id: document.id,
+      fullName: data["FullName"],
+      email: data["Email"],
+      phoneNo: data["Phone Number"],
+      password: data["Password"],
+    );
+  }
+}
