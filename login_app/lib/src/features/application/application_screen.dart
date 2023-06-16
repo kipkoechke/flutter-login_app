0a3779077fa211e7b25d51d0ef141269110a649f
kipkoechke <chepkwonyke1@gmail.com>
2023-06-16 17:44:05 +0300

    Worked on welcome page for applicants

--- login_app/lib/src/features/application/application_screen.dart
+++ login_app/lib/src/features/application/application_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:login_app/src/repository/authentication_repository/authentication_reposirtory.dart';
+import 'package:login_app/src/constants/sizes.dart';
+import 'package:login_app/src/features/application/screens/locality/locality_details_screen.dart';
+import 'package:login_app/src/features/authentication/screens/profile/profile_screen.dart';
 
 class ApplicationScreen extends StatelessWidget {
   const ApplicationScreen({super.key});
@@ -14,19 +17,36 @@ class ApplicationScreen extends StatelessWidget {
           actions: [
             IconButton(
               onPressed: () {
-                AuthenticationRepository.instance.logOut();
+                Get.to(() => const ProfileScreen());
               },
               icon: const Icon(Icons.logout),
             ),
           ],
         ),
-        drawer: Container(),
-        body: Center(
-          child: Text(
-            'Bursary Application Form',
-            style: GoogleFonts.montserrat(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
+        body: Container(
+          padding: const EdgeInsets.all(20),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Bursary Application Form',
+                  style: GoogleFonts.montserrat(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: bDefaultSize),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Get.to(() => const LocalityDetailsScreen());
+                    },
+                    child: const Text('Click to apply'),
+                  ),
+                ),
+              ],
             ),
           ),
         ),
