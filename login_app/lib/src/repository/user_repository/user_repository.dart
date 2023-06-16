faa4c52c548a01aa92b38f6c4bd0cbbdf5a99a5b
kipkoechke <chepkwonyke1@gmail.com>
2023-06-16 17:30:14 +0300

    Worked on database logic

---
+++ login_app/lib/src/repository/user_repository/user_repository.dart
@@ -0,0 +1,149 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:login_app/src/features/application/models/application_model.dart';
+import 'package:login_app/src/features/application/models/locality_model.dart';
+import 'package:login_app/src/features/application/models/personal_model.dart';
+import 'package:login_app/src/features/application/models/school_model.dart';
+import 'package:login_app/src/features/authentication/models/user_model.dart';
+
+class UserRepository extends GetxController {
+  static UserRepository get instance => Get.find();
+
+  final _db = FirebaseFirestore.instance;
+
+//-- Store data in the firestore
+  Future<void> createUser(UserModel user) async {
+    await _db
+        .collection("Users")
+        .add(user.toJson())
+        .whenComplete(
+          () => Get.snackbar(
+            'Success',
+            'Your account has been created',
+            snackPosition: SnackPosition.BOTTOM,
+            backgroundColor: Colors.green.withOpacity(0.1),
+            colorText: Colors.green,
+          ),
+        )
+        .catchError((error, stackTrace) {
+      Get.snackbar('Error', 'Something went wrong. Try again',
+          snackPosition: SnackPosition.BOTTOM,
+          backgroundColor: Colors.redAccent.withOpacity(0.1),
+          colorText: Colors.red);
+      throw error;
+    });
+  }
+
+//-- Fetch data from the firestore
+  Future<UserModel> getUserDetails(String email) async {
+    final snapshot =
+        await _db.collection("Users").where("Email", isEqualTo: email).get();
+    final userData = snapshot.docs.map((e) => UserModel.fromSnapshot(e)).single;
+    return userData;
+  }
+
+  Future<List<UserModel>> allUsers() async {
+    final snapshot = await _db.collection("Users").get();
+    final userData =
+        snapshot.docs.map((e) => UserModel.fromSnapshot(e)).toList();
+    return userData;
+  }
+
+  //-- Application logic
+  Future<void> createApplication(ApplicationModel application) async {
+    await _db
+        .collection("Users")
+        .doc("application")
+        .collection("userapplication")
+        .add(application.toJson())
+        .whenComplete(
+          () => Get.snackbar(
+            'Success',
+            'Your account has been created',
+            snackPosition: SnackPosition.BOTTOM,
+            backgroundColor: Colors.green.withOpacity(0.1),
+            colorText: Colors.green,
+          ),
+        )
+        .catchError((error, stackTrace) {
+      Get.snackbar('Error', 'Something went wrong. Try again',
+          snackPosition: SnackPosition.BOTTOM,
+          backgroundColor: Colors.redAccent.withOpacity(0.1),
+          colorText: Colors.red);
+      throw error;
+    });
+  }
+
+  Future<void> createLocation(LocalityModel locality) async {
+    await _db
+        .collection("Users")
+        .doc("application")
+        .collection("locality")
+        .add(locality.toJson())
+        .whenComplete(
+          () => Get.snackbar(
+            'Success',
+            'Your account has been created',
+            snackPosition: SnackPosition.BOTTOM,
+            backgroundColor: Colors.green.withOpacity(0.1),
+            colorText: Colors.green,
+          ),
+        )
+        .catchError((error, stackTrace) {
+      Get.snackbar('Error', 'Something went wrong. Try again',
+          snackPosition: SnackPosition.BOTTOM,
+          backgroundColor: Colors.redAccent.withOpacity(0.1),
+          colorText: Colors.red);
+      throw error;
+    });
+  }
+
+  Future<void> createSchool(SchoolModel school) async {
+    await _db
+        .collection("Users")
+        .doc("application")
+        .collection("school")
+        .add(school.toJson())
+        .whenComplete(
+          () => Get.snackbar(
+            'Success',
+            'Your account has been created',
+            snackPosition: SnackPosition.BOTTOM,
+            backgroundColor: Colors.green.withOpacity(0.1),
+            colorText: Colors.green,
+          ),
+        )
+        .catchError((error, stackTrace) {
+      Get.snackbar('Error', 'Something went wrong. Try again',
+          snackPosition: SnackPosition.BOTTOM,
+          backgroundColor: Colors.redAccent.withOpacity(0.1),
+          colorText: Colors.red);
+      throw error;
+    });
+  }
+
+  Future<void> createPersonal(PersonalModel personal) async {
+    await _db
+        .collection("Users")
+        .doc("application")
+        .collection("personal")
+        .add(personal.toJson())
+        .whenComplete(
+          () => Get.snackbar(
+            'Success',
+            'Your account has been created',
+            snackPosition: SnackPosition.BOTTOM,
+            backgroundColor: Colors.green.withOpacity(0.1),
+            colorText: Colors.green,
+          ),
+        )
+        .catchError((error, stackTrace) {
+      Get.snackbar('Error', 'Something went wrong. Try again',
+          snackPosition: SnackPosition.BOTTOM,
+          backgroundColor: Colors.redAccent.withOpacity(0.1),
+          colorText: Colors.red);
+      throw error;
+    });
+  }
+}
