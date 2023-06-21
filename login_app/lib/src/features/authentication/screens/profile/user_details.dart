75a84b2577426c5609e246d06eca93d1d93cfe48
kipkoechke <chepkwonyke1@gmail.com>
2023-06-21 18:58:11 +0300

    Added a screen to display when a user detail is tapped

---
+++ login_app/lib/src/features/authentication/screens/profile/user_details.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class UserDetailsScreen extends StatelessWidget {
+  const UserDetailsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("You've made it!"),
+      ),
+    );
+  }
+}
