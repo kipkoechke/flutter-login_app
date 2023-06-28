9504739afe7878c5090bca7735e22ffb2be22d48
kipkoechke <chepkwonyke1@gmail.com>
2023-06-28 09:53:33 +0300

    Added a circular progress indicator as the initial screen

--- login_app/lib/main.dart
+++ login_app/lib/main.dart
@@ -2,7 +2,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:login_app/firebase_options.dart';
-import 'package:login_app/src/features/authentication/screens/splash_screen/splash_screen.dart';
 import 'package:login_app/src/repository/authentication_repository/authentication_reposirtory.dart';
 import 'package:login_app/src/utils/theme/theme.dart';
 
@@ -26,7 +25,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       defaultTransition: Transition.leftToRightWithFade,
       transitionDuration: const Duration(milliseconds: 500),
-      home: const SplashScreen(),
+      home: const CircularProgressIndicator(),
     );
   }
 }
