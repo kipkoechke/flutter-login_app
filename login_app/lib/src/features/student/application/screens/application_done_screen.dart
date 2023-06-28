08bf65ba9a15aad512a7a9dd8dfb5c8a40750852
kipkoechke <chepkwonyke1@gmail.com>
2023-06-28 10:02:14 +0300

    Added application completion screen

---
+++ login_app/lib/src/features/student/application/screens/application_done_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:login_app/src/features/student/application/screens/application_status.dart';
+
+class ApplicationDoneScreen extends StatelessWidget {
+  const ApplicationDoneScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Center(
+            child: Text(
+              " Your application was submited successfully",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ),
+          OutlinedButton(
+              onPressed: () {
+                Get.to(() => ApplicationStatusScreen());
+              },
+              child: const Text('Application Status')),
+        ],
+      ),
+    );
+  }
+}
