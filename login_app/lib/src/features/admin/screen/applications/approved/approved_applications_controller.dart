407c0585ea5022204d2f34abd9d08527e0d000be
kipkoechke <chepkwonyke1@gmail.com>
2023-06-30 17:27:15 +0300

    Worked on approved applications screen

---
+++ login_app/lib/src/features/admin/screen/applications/approved/approved_applications_controller.dart
@@ -0,0 +1,19 @@
+import 'package:get/get.dart';
+import 'package:login_app/src/features/student/application/models/application_form_model.dart';
+import 'package:login_app/src/repository/user_repository/user_repository.dart';
+
+class ApprovedApplicationController extends GetxController {
+  static ApprovedApplicationController get instance => Get.find();
+
+  final String title = 'Approved Applications';
+  final _userRepo = Get.put(UserRepository());
+
+  approveApplication(String id) async {
+    await _userRepo.approveUserApplication(id);
+  }
+
+  //-- Retrieves user application data
+  Stream<List<ApplicationFormModel>> getApprovedApplication() {
+    return _userRepo.approvedApplication().asStream();
+  }
+}
