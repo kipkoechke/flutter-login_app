7a8129bb0e5c1bf080e087853e3b5dd86cd72530
kipkoechke <chepkwonyke1@gmail.com>
2023-07-04 07:26:49 +0300

    Modified allocation status messages

--- login_app/lib/src/features/student/application/screens/student_application_status/student_application_status_controller.dart
+++ login_app/lib/src/features/student/application/screens/student_application_status/student_application_status_controller.dart
@@ -11,16 +11,16 @@ class StudentApplicationStatusController extends GetxController {
     final allocationStatus = await _userRepo.getAllocationStatus();
     if (allocationStatus == 'No Application') {
       // Display appropriate message for no application
-      return ("You have not submitted any application.");
+      return ("Ouch!\nYou have not submitted any application.");
     } else if (allocationStatus == 'Pending') {
       // Display appropriate message for pending application
-      return ("Your application is pending review.");
+      return ("Reviewing!\nYour application is pending review.");
     } else if (allocationStatus == 'Approved') {
       // Display appropriate message for approved application
-      return ("Congratulations! Your application has been approved.");
+      return ("Congratulations!\nYour application has been approved.");
     } else if (allocationStatus == 'Declined') {
       // Display appropriate message for declined application
-      return ("Sorry, your application has been declined.");
+      return ("Sorry!\nYour application has been declined.");
     } else {
       // Handle any other status if needed
       return ("Unknown application status: $allocationStatus");
