43c0aec2a5c300f57137d27d9fbafa2f674b1ace
kipkoechke <chepkwonyke1@gmail.com>
2023-07-05 16:01:47 +0300

    Modified application screen

--- login_app/lib/src/features/student/application/screens/student_home_page/application_screen.dart
+++ login_app/lib/src/features/student/application/screens/student_home_page/application_screen.dart
@@ -6,6 +6,7 @@ import 'package:login_app/src/features/admin/screen/bursaries/bursary_controller
 import 'package:login_app/src/features/authentication/screens/profile/profile_screen.dart';
 import 'package:login_app/src/features/student/application/screens/student_application_form/application_form_screen.dart';
 
+
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
@@ -32,14 +33,6 @@ class HomePage extends StatelessWidget {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                // Text(
-                //   'Bursary Application Form',
-                //   style: GoogleFonts.montserrat(
-                //     fontSize: 20,
-                //     fontWeight: FontWeight.bold,
-                //   ),
-                // ),
-                
                 const SizedBox(height: bDefaultSize),
                 const Text(
                   'Available Bursaries',
@@ -48,52 +41,66 @@ class HomePage extends StatelessWidget {
                 const SizedBox(height: 8.0),
                 Expanded(
                   child: Obx(
-                    () => ListView.builder(
-                      itemCount: bursaryController.bursaries.length,
-                      itemBuilder: (context, index) {
-                        final bursary = bursaryController.bursaries[index];
-                        return Card(
-                          child: ListTile(
-                            onTap: () {
-                              bursaryController.setBursaryClicked(bursary);
-                              Get.to(() => const StudentApplicationForm());
-                            },
-                            title: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text("Title: ${bursary.title}"),
-                                const Divider(),
-                                Text(bursary.description),
-                                const Divider()
-                              ],
-                            ),
-                            subtitle: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  "Deadline: ${DateFormat('MMM dd, yyyy -  HH:mm a').format(bursary.deadline)}",
-                                ),
-                                const Divider(),
-                                SizedBox(
-                                  width: double.infinity,
-                                  child: ElevatedButton(
-                                    onPressed: () {
-                                      bursaryController
-                                          .setBursaryClicked(bursary);
-                                      Get.to(
-                                          () => const StudentApplicationForm());
-                                    },
-                                    child: const Text(
-                                      'Proceed to apply',
-                                    ),
-                                  ),
-                                ),
-                              ],
+                    () {
+                      if (bursaryController.bursaries.isEmpty) {
+                        return const Center(
+                          child: Text(
+                            'No bursaries available',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
                             ),
                           ),
                         );
-                      },
-                    ),
+                      } else {
+                        return ListView.builder(
+                          itemCount: bursaryController.bursaries.length,
+                          itemBuilder: (context, index) {
+                            final bursary = bursaryController.bursaries[index];
+                            return Card(
+                              child: ListTile(
+                                onTap: () {
+                                  bursaryController.setBursaryClicked(bursary);
+                                  Get.to(() => const StudentApplicationForm());
+                                },
+                                title: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text("Title: ${bursary.title}"),
+                                    const Divider(),
+                                    Text(bursary.description),
+                                    const Divider()
+                                  ],
+                                ),
+                                subtitle: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      "Deadline: ${DateFormat('MMM dd, yyyy -  HH:mm a').format(bursary.deadline)}",
+                                    ),
+                                    const Divider(),
+                                    SizedBox(
+                                      width: double.infinity,
+                                      child: ElevatedButton(
+                                        onPressed: () {
+                                          bursaryController
+                                              .setBursaryClicked(bursary);
+                                          Get.to(() =>
+                                              const StudentApplicationForm());
+                                        },
+                                        child: const Text(
+                                          'Proceed to apply',
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            );
+                          },
+                        );
+                      }
+                    },
                   ),
                 ),
               ],
