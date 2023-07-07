2b3af6b7ab0403ed67dbd88f8843d243b6e89b9d
kipkoechke <chepkwonyke1@gmail.com>
2023-07-07 22:38:00 +0300

    Added some packages

--- login_app/lib/main.dart
+++ login_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:login_app/firebase_options.dart';
@@ -7,17 +8,47 @@ import 'package:login_app/src/features/admin/screen/applications/applications_st
 import 'package:login_app/src/repository/authentication_repository/authentication_reposirtory.dart';
 import 'package:login_app/src/utils/theme/theme.dart';
 
+// Listening to the background messages
+@pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  print("Handling a background message: ${message.messageId}");
+}
+
+FirebaseMessaging messaging = FirebaseMessaging.instance;
+// RxString deviceToken = RxString('');
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform)
-      .then((value) => Get.put(AuthenticationRepository()));
+      .then((value) {
+    messaging.getInitialMessage();
+    FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+    Get.put(AuthenticationRepository());
+  });
+
+  // try {
+  //   deviceToken.value = (await messaging.getToken())!;
+  //   print('Device Token: ${deviceToken.value}');
+  // } catch (e) {
+  //   print('Error retrieving device token: $e');
+  // }
+
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+// class InitialBinding implements Bindings {
+//   @override
+//   void dependencies() {
+//     Get.put(AuthenticationRepository());
+//     Get.put(deviceToken, permanent: true);
+//   }
+// }
 
-  // This widget is the root of your application.
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
@@ -35,6 +66,7 @@ class MyApp extends StatelessWidget {
           binding: ApplicationStatusBinding(),
         ),
       ],
+      // initialBinding: InitialBinding(),
     );
   }
 }
