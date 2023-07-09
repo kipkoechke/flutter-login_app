4593bdc77aca14fb658353de904d78b54a4538b4
kipkoechke <chepkwonyke1@gmail.com>
2023-07-09 17:30:44 +0300

    Added navigation to approved screen

--- login_app/lib/src/features/admin/screen/applications/approved/total_approved_applications.dart
+++ login_app/lib/src/features/admin/screen/applications/approved/total_approved_applications.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:login_app/src/features/admin/screen/applications/approved/approved_applications_controller.dart';
+import 'package:login_app/src/features/admin/screen/applications/approved/approved_applications_screen.dart';
 
 class TotalNumberOfApprovedApplications extends StatelessWidget {
   const TotalNumberOfApprovedApplications({
@@ -16,9 +18,34 @@ class TotalNumberOfApprovedApplications extends StatelessWidget {
       builder: (context, snapshot) {
         if (snapshot.hasData) {
           final approvedApplicationsCount = snapshot.data!;
-          return Text(
-            'Approved Applications: $approvedApplicationsCount',
-            style: const TextStyle(fontSize: 16),
+          return GestureDetector(
+            onTap: () {
+              Get.to(const ApprovedApplicationScreen());
+            },
+            child: Card(
+              color: Colors.green,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    const Text(
+                      'Approved ',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      '$approvedApplicationsCount',
+                      style: const TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
           );
         } else if (snapshot.hasError) {
           return Text(
