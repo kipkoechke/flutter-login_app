d7057c9fa272c6a3725de2a2cfca448b3f1b4965
kipkoechke <chepkwonyke1@gmail.com>
2023-07-09 17:31:31 +0300

    Added a navigation to declined screen

--- login_app/lib/src/features/admin/screen/applications/declined/total_declined_applications.dart
+++ login_app/lib/src/features/admin/screen/applications/declined/total_declined_applications.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:login_app/src/features/admin/screen/applications/declined/declined_applications_controller.dart';
+import 'package:login_app/src/features/admin/screen/applications/declined/declined_applications_screen.dart';
 
 class TotalNumberOfDeclinedApplications extends StatelessWidget {
   const TotalNumberOfDeclinedApplications({
@@ -16,9 +18,34 @@ class TotalNumberOfDeclinedApplications extends StatelessWidget {
       builder: (context, snapshot) {
         if (snapshot.hasData) {
           final declinedApplicationsCount = snapshot.data!;
-          return Text(
-            'Declined Applications: $declinedApplicationsCount',
-            style: const TextStyle(fontSize: 16),
+          return GestureDetector(
+            onTap: () {
+              Get.to(const DeclinedApplicationsScreen());
+            },
+            child: Card(
+              color: Colors.red,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    const Text(
+                      'Declined ',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      '$declinedApplicationsCount',
+                      style: const TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
           );
         } else if (snapshot.hasError) {
           return Text(
