62fe035dff9b925d002bd3f168a0af807d75f9a6
kipkoechke <chepkwonyke1@gmail.com>
2023-07-09 17:33:09 +0300

    Added a navigation to pending screen

--- login_app/lib/src/features/admin/screen/applications/pending/total_pending_applications.dart
+++ login_app/lib/src/features/admin/screen/applications/pending/total_pending_applications.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:login_app/src/features/admin/screen/applications/pending/pending_applications_controller.dart';
+import 'package:login_app/src/features/admin/screen/applications/pending/pending_applications_screen.dart';
 
 class TotalNumberOfPendingApplications extends StatelessWidget {
   const TotalNumberOfPendingApplications({
@@ -16,9 +18,34 @@ class TotalNumberOfPendingApplications extends StatelessWidget {
       builder: (context, snapshot) {
         if (snapshot.hasData) {
           final pendingApplicationsCount = snapshot.data!;
-          return Text(
-            'Pending Applications: $pendingApplicationsCount',
-            style: const TextStyle(fontSize: 16),
+          return GestureDetector(
+            onTap: () {
+              Get.to(const PendingApplicationsScreen());
+            },
+            child: Card(
+              color: Colors.orange,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    const Text(
+                      'Pending ',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      '$pendingApplicationsCount',
+                      style: const TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
           );
         } else if (snapshot.hasError) {
           return Text(
