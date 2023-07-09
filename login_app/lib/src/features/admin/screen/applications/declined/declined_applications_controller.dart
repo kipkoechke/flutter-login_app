e9f576bea89e25bfee03ec6e9480866b797cd5c8
kipkoechke <chepkwonyke1@gmail.com>
2023-07-09 17:31:10 +0300

    Added reson for declining application

--- login_app/lib/src/features/admin/screen/applications/declined/declined_applications_controller.dart
+++ login_app/lib/src/features/admin/screen/applications/declined/declined_applications_controller.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:login_app/src/features/student/application/models/application_form_model.dart';
 import 'package:login_app/src/repository/user_repository/user_repository.dart';
@@ -8,8 +9,12 @@ class DeclinedApplicationsController extends GetxController {
   final String title = 'Declined Applications';
   final _userRepo = Get.put(UserRepository());
 
-  declineApplication(String id) async {
-    await _userRepo.declineUserApplication(id);
+  declineApplication(String id, String reason) async {
+    await _userRepo.declineUserApplication(id, reason);
+  }
+
+  Future<String> getDeclineReason() async {
+    return await _userRepo.declineReason();
   }
 
   //-- Retrieves user application data
@@ -21,5 +26,41 @@ class DeclinedApplicationsController extends GetxController {
     return _userRepo.declinedApplicationsCount().asStream();
   }
 
-  
+  void showDeclineReasonDialog(String applicationId) {
+    String reason = '';
+
+    Future.delayed(Duration.zero, () {
+      Get.dialog(
+        AlertDialog(
+          title: const Text('Decline Reason'),
+          content: TextField(
+            onChanged: (value) {
+              reason = value;
+            },
+            decoration: const InputDecoration(
+              hintText: 'Enter reason here',
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Get.back();
+              },
+              child: const Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () {
+                final DeclinedApplicationsController declineController =
+                    Get.find();
+                declineController.declineApplication(applicationId, reason);
+                Get.back();
+              },
+              child: const Text('Decline'),
+            ),
+          ],
+        ),
+      );
+    });
+  }
+
 }
